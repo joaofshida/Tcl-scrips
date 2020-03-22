5f4ffd05be9c78f393067c4321684285d15f5020
joaofshida <62485953+joaofshida@users.noreply.github.com>
2020-03-21 19:48:27 -0500

    Update Bbarrel-radius.tcl

--- Bbarrel-radius.tcl
+++ Bbarrel-radius.tcl
@@ -12,20 +12,20 @@ proc barrelRadius {T outfile} {
   set outfile [open $outfile w]
 	set mol top
   set nSteps [molinfo $mol get numframes]
-  
+
   # Initialize list of beta occurence
   set sel [atomselect top "name CA"]
   set struct [$sel get structure]
   set len [llength $struct]
   set betaList {}
   for {set i 0} {$i <= $len} {incr i} {
-      lappend betaList 0  
-    }
-    
+      lappend betaList 0
+  }
+
   # For every Frame
   for {set i 0} {$i <= $nSteps} {incr i} {
     # Get structure list for this frame
-	  animate top goto $i
+    animate top goto $i
     mol ssrecalc top
     set thisstruc [$sel get structure]
     set len [llength $thisstruc]
@@ -34,26 +34,38 @@ proc barrelRadius {T outfile} {
       set struct [lindex $thisstruc $i]
       if {$struct == "B"} {
       set previousCount [llindex betaList $i]
-      lset betaList $i [$previousCount + (1/$nSteps)]
+      lset betaList $i [expr $previousCount + (1/$nSteps)]
       } else {
       }
     }
-   set betaIndexList {}
-   for {set i 0} {$i <= $nSteps} {incr i} {
-    set ammountOfBeta [lindex betaList $i] 
+
+
+  set betaIndexList {}
+  for {set i 0} {$i <= $nSteps} {incr i} {
+    set ammountOfBeta [lindex betaList $i]
     if {$ammountOfBeta >= %T}{
-      lappend betaIndexList $i
+        lappend betaIndexList $i
     } else {}
-    
+  }
+
+  set lengthOfResidues [llength betaIndexList]
+  # For every Frame
+  for {set i 0} {$i <= $nSteps} {incr i} {
+    set frameSum 0
+    for {set j 0} {$j <= lengthOfResidues} {incr j} {
+        set resNum [lindex $betaIndexList $j]
+        set residueSel [atomselect top "backbone and resid $resNum"]
+        set pos [center $residueSel]
+        set x [lindex $pos 0]
+        set y [lindex $pos 1]
+        set x2 [expr $x*$x]
+        set y2 [expr $y*$y]
+        # Assumption: z axis is central axis of barrel
+        set dOverSteps [expr { sqrt($x2 + $y2)/$nSteps }]
+        set frameSum [expr $frameSum + $dOverSteps]
+    puts $outfile "$i\t$frameSum"
     }
-    
-    
-		$sel1 frame $i
-		$sel2 frame $i
-		set centerOfMass1 [measure center $sel1 weight mass]
-		set centerOfMass2 [measure center $sel2 weight mass]
-		set difference [vecdist $centerOfMass1 $centerOfMass2]
-		puts $outfile "$i\t$difference"
-	}
+  }
+  }
 	close $outfile
 }
